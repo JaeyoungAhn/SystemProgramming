3576045e575e6f0b9a4b965d0a35606155efcfaa
JaeyoungAhn <quaki@naver.com>
2022-11-11 00:03:48 +0000

    update

---
+++ lab10_s2020118082/smsh4/varlib.h
@@ -0,0 +1,16 @@
+// *varlib.h*
+// declares environment—related functions 
+
+// keep val associated with var
+int VLstore(char* name, char* val);
+// return value of var
+char* VLlookup(char* name);
+// adds name to list of env vars
+int VLexport(char* name);
+// performs the shell's set command
+void VLlist();
+// copy from environ to table
+int VLenviron2table(char* env[]);
+// copy from table to environ
+char** VLtable2environ(); 
+
