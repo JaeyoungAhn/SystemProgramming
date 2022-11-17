9815bb17921679a3bdccf3861c8217918ad8c5a6
JaeyoungAhn <quaki@naver.com>
2022-11-17 11:42:05 +0000

    update

--- lab11_s2020118082/lab11_s2020118082/pipe.c
+++ lab11_s2020118082/lab11_s2020118082/pipe.c
@@ -1,5 +1,12 @@
-/* sortfromfile.c
- * sorts the input data and outputs
+/* pipe.c
+ *			* Demonstrates how to create a pipeline from one process to another
+ *			* Takes two args, each a command, and connects
+ *			  av[1]'s output to input of av[2]
+ *			* usage: pipe command1 command2
+ *			  effect: command1 | command2
+ *			* Limitations: commands do not takes arguments
+ *			* uses execlp() since known number of args
+ *			* Note: exchange child and parent and watch fun
  */
 
 #include <stdio.h>
@@ -16,15 +23,13 @@ int main(int ac, char **av) {
 		newfd,			/* useful for pipes */
 		pid;			/* and the pid	*/
 
-	if ( ac != 2 ){
-		fprintf(stderr, "usage: sortfromfile [.txt]\n");
+	if ( ac != 3 ){
+		fprintf(stderr, "usage: pipe cmd1 cmd2\n");
 		exit(1);
 	}
+	if(pipe(thepipe) == -1 )	/* get a pipe	*/
+		oops("Cannot get a pipe", 1);
 	
-	printf("About to run sort with input from %s\n", av[1]);
-
-	if(pipe(thepipe) == -1 )	/* get a pipe	*/	
-			oops("Cannot get a pipe", 1);	
 	/* ---------------------------------------------------- */
 	/*   now we have a pipe, now let's get two processes   */
 
@@ -34,36 +39,28 @@ int main(int ac, char **av) {
 	/* --------------------------------------------------- */
 	/*		Right Here, there are two processes			   */
 	/*			parent will read from pipe				   */
-	if( pid > 0 ){	/* parent will exec sort	*/
+
+	if( pid > 0 ){	/* parent will exec av[2]	*/
 		close(thepipe[1]);	/* parent doesn't wirte to pipe	*/
 		
-		if( (pid = fork()) == -1 ) /* get a proc */
-			oops("Cannot fork", 2);
-
-		if( pid == 0 ) {
-			if( dup2(thepipe[0], 0) == -1 )
-				oops("could not redirect stdin", 3);
-			close(thepipe[0]);	/* stdin is duped, close pipe	*/
-			execlp("sort", "sort", NULL);
-			oops("sort", 4);
-			exit(1);
-		}
-		else {
-			close(thepipe[0]);
-			wait(NULL);
-			printf("Done running sort < %s\n", av[1]);
-			exit(1);
-		}
+		if( dup2(thepipe[0], 0) == -1 )
+			oops("could not redirect stdin", 3);
+		
+		close(thepipe[0]);	/* stdin is duped, close pipe	*/
+		execlp( av[2], av[2], NULL );
+		oops(av[2], 4);
 	}
-		/* child execs av[1] and writes into pipe		*/
+
+	/* child execs av[1] and writes into pipe		*/
 	
 	close(thepipe[0]);		/* child doesn't read from pipe */
+
 	if( dup2(thepipe[1], 1) == -1 )
 		oops("could not redirect stdout", 4);
 
 	close(thepipe[1]);	/* stdout is duped, close pipe	*/
-	
-	execlp("cat", "cat", av[1], NULL);
+	execlp(av[1], av[1], NULL);
 	oops(av[1], 5);
+
 	return 0;
 }
